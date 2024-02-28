e43e76fea969a52effcfd35569641a8be01cb43d
Chamuditha Kavishan Perera <139614066+chamudithaperera@users.noreply.github.com>
2024-02-28 21:42:30 +0530

    Add files via upload

---
+++ Java basic/Ex02_Variables.java
@@ -0,0 +1,26 @@
+class Ex02_Variables
+{
+    public static void main(String[] args)
+    {
+        // byte (number, 1 byte)
+        // short (number, 2 bytes)
+        // int (number, 4 bytes)
+        // long (number, 8 bytes)
+        // float (float number, 4 bytes)
+        // double (float number, 8 bytes)
+        // char (a character, 2 bytes)
+        // boolean (true or false, 1 byte)
+
+        //Exsample
+
+        byte varByte = 1;
+        short varShort = 10;
+        int varInt = 12345;
+        long varLong = 12345;
+        float varFloat = 10;
+        double varDouble = 10.50;
+        char varChar = 'A';
+        boolean varBlooean = true;
+        String varString = "John";
+    }
+}
